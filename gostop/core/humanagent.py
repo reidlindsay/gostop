ac96913a80b22c43260c39b9ef778909bdcad0f3
Reid Lindsay <reidlindsay@rallycraft.com>
2015-04-09 22:07:57 +1000

    Fix import issue.

--- gostop/core/humanagent.py
+++ gostop/core/humanagent.py
@@ -1,12 +1,10 @@
-from future.builtins import input
+from builtins import input
 
 from .agent import Agent
 
 
 class HumanAgent(Agent):
     def get_action(self, state, possible_actions):
-        #print(u"Player {0} has hand {1}".format(
-            #u'Test1', u'Test2')) #, self.name)) #self.hand))
         while True:
             response = input(u"== Choose action for {0} {1}? ".format(
                 self.name,
